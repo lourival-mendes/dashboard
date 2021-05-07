83692ab561fbe2981b1ac36f95d86c179ddb81d2
lourival-mendes <minhavirtude@gmail.com>
2021-05-07 13:09:33 -0300

    Security config

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -5,7 +5,9 @@ const NavBar = () => {
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
             <div className="container">
                 <nav className="my-2 my-md-0 mr-md-3">
-                    <img src={ImgDsDark} alt="DevSuperior" width="120" />
+                    <a href="https://lourival-mendes-dashboard.netlify.app/" target="_blank" rel="noopener noreferrer">
+                        <img src={ImgDsDark} alt="DevSuperior" width="120" />
+                    </a>
                 </nav>
             </div>
         </div>
