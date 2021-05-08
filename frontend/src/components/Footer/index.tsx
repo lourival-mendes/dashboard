8b45c7f036d652d53f9512bb670592c946134d5e
lourival-mendes <minhavirtude@gmail.com>
2021-05-08 18:34:11 -0300

    Home, navigation

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -3,6 +3,7 @@ const Footer = () => {
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://github.com/lourival-mendes" target="_blank" rel="noreferrer">Lourival Mendes</a></p>
+                <p>Disponível em <a href="https://lourival-mendes-dashboard.netlify.app/" target="_blank" rel="noopener noreferrer">https://lourival-mendes-dashboard.netlify.app</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
             Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
